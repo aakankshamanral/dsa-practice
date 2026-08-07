5fa4209bd31bb18ac5cfe0ff0084afb16644ad23
Aakanksha Manral <aakanksha.manral@gmail.com>
2026-08-07 22:57:34 +0530

    Time: 5 ms (70.53%) | Memory: 16.2 MB (66.07%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.c
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.c
@@ -1,16 +1,24 @@
-int compare( const void *a , const void *b){
-    return ( *(int*)a - *(int*)b );
-}
-
 int maxIceCream(int* costs, int costsSize, int coins) {
-    qsort(costs, costsSize , sizeof(int) , compare);
-    int ans = 0 ;
-    for(int i = 0 ; i <costsSize ; i ++){
-        if(coins>=costs[i]){
-            ans ++;
-            coins-= costs[i];
-        }
-        else break;
+    int max = costs[0];
+    for(int i = 1 ; i < costsSize ; i ++)
+        if (max<costs[i]) max = costs[i];
+
+    int count[max+1];
+    for(int i = 0 ; i <=max ; i++)
+        count[i]=0;
+
+    for(int i = 0 ; i < costsSize ; i++){
+        count[costs[i]]++;
     }
+    
+    int ans = 0 ;
+    for(int price = 1 ; price<=max ; price++ ){
+        while(coins>= price && count[price]>0){
+            coins -=price;
+            count[price]--;
+            ans++;
+        }
+    }
+
     return ans;
 }
